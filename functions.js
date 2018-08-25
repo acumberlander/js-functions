99887dfecb3cf270c42b4b0e441be9f56f2897e4
Austin Cumberlander <austincumberlander@gmail.com>
2018-08-25 11:17:15 -0500

    created nuggetizer

---
+++ functions.js
@@ -0,0 +1,26 @@
+// input = animal ex. fish
+//output = animal product ex. fish stix
+
+function nuggetizer(animal){
+    return `${animal} stix`;
+}
+
+console.log('fish:', nuggetizer('fish'));
+console.log('cat:', nuggetizer('cat'));
+console.log('bear:', nuggetizer('bear'));
+
+const nuggetizer2 = (animal) => {
+    return `${animal} jerky`
+};
+
+console.log('deer:', nuggetizer2('deer'));
+
+
+const nomnom = (person, food) => {
+    return `${person} devoured ${food}`;
+};
+
+const bearSticks = nuggetizer('bear');
+const fishSticks = nuggetizer('fish');
+
+console.log(nomnom('austin', bearSticks)); // austin devoured bear stix
