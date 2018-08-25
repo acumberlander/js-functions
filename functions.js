466f10abf973027bb7ef40dc8908aa09789ef08f
Austin Cumberlander <austincumberlander@gmail.com>
2018-08-25 11:48:37 -0500

    made writeToDom

--- functions.js
+++ functions.js
@@ -24,3 +24,19 @@ const bearSticks = nuggetizer('bear');
 const fishSticks = nuggetizer('fish');
 
 console.log(nomnom('austin', bearSticks)); // austin devoured bear stix
+
+//function numberAdder that takes in anumber and returns the number plus 3.
+// ex. so input=22, output=25
+
+const numberAdder = (num) => {
+    const finalNumber = num + 3;
+    printToDom(`<h2>${finalNumber}</h2>`, 'allTheNumbers')
+};
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+
+numberAdder(22);
+numberAdder(4);
